a9d238101dfe72c963594e83f7e9791c5cc38229
Egor Zhukov <termquso@gmail.com>
2020-04-24 00:10:55 +0300

    Made a level generation system

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class BoardManager : MonoBehaviour {
+
+	[Serializable]
+	public class Count
+	{
+		public int maximum;
+		public int minimum;
+
+		public Count(int min, int max)
+		{
+			maximum = max;
+			minimum = min;
+		}
+	}
+
+	public int rows = 8; // int that save grid columns on which spawn walls, enemy, food
+	public int columns = 8; // int that save grid columns on which spawn walls, enemy, food
+	public Count wallCount = new Count(5, 9); // random count of walls, that blocking your way
+	public Count foodCount = new Count(1, 5); // random count of food
+	public GameObject exit; // exit
+	public GameObject[] floorTiles; // floors
+	public GameObject[] foodTiles;	// food
+	public GameObject[] enemyTiles; // enemy
+	public GameObject[] WallTiles; //Walls
+	public GameObject[] OuterWallTiles; //OuterWalls
+
+	private Transform boardHolder;
+	private List<Vector3> gridPositions = new List<Vector3>(); // save the positions on which will spawn walls, enemy, food
+
+	void InitialiseList()
+	{
+		for (int x = 1; x < columns - 1; x++)
+		{
+			for (int y = 1; y < rows - 1; y++)
+			{
+				gridPositions.Add(new Vector3(x, y, 0f));
+			}
+		}
+	}
+	void BoardSetup()
+	{
+		boardHolder = new GameObject("Board").transform;
+
+		for (int x = -1; x < columns + 1; x++)
+		{
+			for(int y = -1; y < rows + 1; y++)
+			{
+				GameObject toInstantiate = 
+						floorTiles[Random.Range(0, floorTiles.Length)];
+
+				if (x == -1 || x == columns || y == -1 || y == rows)
+				{
+					toInstantiate = 
+						OuterWallTiles[Random.Range(0, OuterWallTiles.Length)];
+				}
+				GameObject instance = Instantiate(toInstantiate,
+					new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+				instance.transform.SetParent(boardHolder); 
+			}
+		}
+	}
+
+	Vector3 RandomPosition()
+	{
+		int randomIndex = Random.Range(0, gridPositions.Count);
+		Vector3 randomPositions = gridPositions[randomIndex];
+		gridPositions.RemoveAt(randomIndex);
+
+		return randomPositions;
+	}
+	void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+	{
+		int ObjectCount = Random.Range(minimum, maximum + 1);
+		for(int i = 0; i < ObjectCount; i++)
+		{
+			Vector3 randomPosition = RandomPosition();
+			GameObject tileChoise = tileArray[Random.Range(0, tileArray.Length)];
+
+			Instantiate(tileChoise, randomPosition, Quaternion.identity);
+		}
+	}
+	public void SetupScene(int level)
+	{
+		BoardSetup();
+		InitialiseList();
+		LayoutObjectAtRandom(WallTiles,
+					wallCount.minimum, wallCount.maximum);
+		LayoutObjectAtRandom(foodTiles,
+					foodCount.minimum, foodCount.maximum);
+
+		int enemyCount = (int)Mathf.Log(level, 2f);
+		LayoutObjectAtRandom(enemyTiles,
+					enemyCount, enemyCount);
+		Instantiate(exit,
+				new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public BoardManager BoardScript;
+	private int level = 6;
+
+	void Awake()
+	{
+		BoardScript = GetComponent<BoardManager>();
+		InitGame();
+	}
+
+	void InitGame()
+	{
+		BoardScript.SetupScene(level);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
